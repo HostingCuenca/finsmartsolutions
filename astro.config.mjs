2e17553ebb1b323068a8d1c41299477d55754659
HostingCuenca <42749056+roggrt@users.noreply.github.com>
2025-03-18 08:59:15 -0500

    aja

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import tailwind from '@astrojs/tailwind';
 import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  site: 'https://finmartsolutions.com',
+  site: 'https://finmartsolutions.info',
   integrations: [
     tailwind(),
     sitemap()
