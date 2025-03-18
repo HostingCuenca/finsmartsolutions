1c93955e46368c3da64db8d96596b938e9cec59e
HostingCuenca <42749056+roggrt@users.noreply.github.com>
2025-03-18 09:09:56 -0500

    correccion

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import sitemap from '@astrojs/sitemap';
 // import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  site: 'https://finmartsolutions.info',
+  site: 'https://finsmartsolutions.info',
   integrations: [
     tailwind(),
     sitemap()
